fc5f700e35ded12207904af07a5000851bb62ba8
tluwaniMS <tluwani2020@gmail.com>
2021-09-22 05:26:06 +0200

    initial-commit

---
+++ robot-lights-first-attempt.ino
@@ -0,0 +1,28 @@
+
+const int redLight = 13;
+const int orangeLight = 12;
+const int greenLight = 11;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(redLight, OUTPUT);
+  pinMode(orangeLight, OUTPUT);
+  pinMode(greenLight, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(redLight, HIGH);
+  delay(4000);
+  digitalWrite(redLight, LOW);
+  delay(1000);
+  digitalWrite(orangeLight, HIGH);
+  delay(4000);
+  digitalWrite(orangeLight, LOW);
+  delay(1000);
+  digitalWrite(greenLight, HIGH);
+  delay(4000);
+  digitalWrite(greenLight, LOW);
+  delay(1000);
+}
